6d263dc13d13cdaeaa8a114c4771eefcf81bebe3
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-05-23 02:12:11 +0300

    Update models.py

--- project/project_app/models.py
+++ project/project_app/models.py
@@ -7,30 +7,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
-    status_book = [
-        ('available', 'available'),
-        ('borrowed', 'borrowed'),
-    ]
-    name = models.CharField(max_length=100, verbose_name='Book Name')
-    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Price')
-    description = models.TextField(verbose_name='Description')
-    id = models.IntegerField(verbose_name='Book ID', primary_key=True)
-    author = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    img = models.ImageField(upload_to='photos/%y/%m/%d', null=True, blank=True)
-    # available = models.BooleanField(default=True)
-    status = models.CharField(max_length=50, choices=status_book, default='available', verbose_name='Status')
-
-    def __str__(self):
-        return self.name
-    
-    # class Meta:
-    #     verbos_name = 'Book'
-    #     verbose_name_plural = 'Books'
-    #     ordering = ['name']
-
-
 class Checkout(models.Model):
     CARD_TYPE_CHOICES = [
         ('Visa', 'Visa'),
@@ -51,7 +27,24 @@ class Checkout(models.Model):
         verbose_name = 'Checkout'
         verbose_name_plural = 'Checkouts'
         ordering = ['cardholder_name']
+      
+class Book(models.Model):
+    status_book = [
+        ('available', 'available'),
+        ('borrowed', 'borrowed'),
+    ]
+    name = models.CharField(max_length=100, verbose_name='Book Name')
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Price')
+    description = models.TextField(verbose_name='Description')
+    id = models.IntegerField(verbose_name='Book ID', primary_key=True)
+    author = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    img = models.ImageField(upload_to='photos/%y/%m/%d', null=True, blank=True)
+    status = models.CharField(max_length=50, choices=status_book, default='available', verbose_name='Status')
 
+    def __str__(self):
+        return self.name
+    
 class Signup(models.Model):
     username = models.CharField(max_length=20)
     email = models.CharField(max_length=40)
@@ -60,6 +53,9 @@ class Signup(models.Model):
     isAdmin = models.BooleanField(default=False)
     profilePicture = models.ImageField(default='default.jpg', upload_to='profilePictures')
 
-
     def __str__(self):
         return self.username
+
+class Profile(models.Model):
+    user = models.OneToOneField(Signup, on_delete=models.CASCADE)
+    wishlist = models.ManyToManyField(Book)
